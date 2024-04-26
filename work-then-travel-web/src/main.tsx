1a2497744ae6651faeee35f7d124c4eee74f8bf5
vivienknecht <knechtvivien@gmail.com>
2024-04-26 14:45:42 +0300

    home page design

--- work-then-travel-web/src/main.tsx
+++ work-then-travel-web/src/main.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import './index.css';
-import HeaderComponent from './components/appbar';
-import Footer from './components/footer';
-
+import HomePage from './components/home-page';
 
 const router = createBrowserRouter([{
 path: "/",
-element: <HeaderComponent />
-},{
-path: "/fot",
-element: <Footer />
+element: <HomePage />
 },
 ])
 
