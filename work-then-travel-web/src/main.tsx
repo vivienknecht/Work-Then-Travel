914d9d6a6ccc73e2b518379aa16bb6aa8bbe9af2
vivienknecht <knechtvivien@gmail.com>
2024-04-26 12:50:04 +0300

    footer first draft

--- work-then-travel-web/src/main.tsx
+++ work-then-travel-web/src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import './index.css';
 import HeaderComponent from './components/appbar';
+import Footer from './components/footer';
 
 
 const router = createBrowserRouter([{
 path: "/",
 element: <HeaderComponent />
+},{
+path: "/fot",
+element: <Footer />
 },
-
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
