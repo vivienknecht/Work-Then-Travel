982c3ede56a847136be681110e92d716e5882520
vivienknecht <knechtvivien@gmail.com>
2024-05-08 19:27:33 +0300

    contact us page

--- work-then-travel-web/src/main.tsx
+++ work-then-travel-web/src/main.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css';
 import HomePage from './components/home-page';
 import Login from './components/login';
 import Signup from './components/signup';
 import AboutUs from './components/about-us-page';
+import ContactUs from './components/contact-us-page';
 
 const router = createBrowserRouter([
-{
-path: "/",
-element: <HomePage />
-},
-{
-  path: "/login",
-  element: <Login />
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/login",
+    element: <Login />
   },
   {
     path: "/signup",
     element: <Signup />
-    },
-    {
-      path: "/about-us",
-      element: <AboutUs />
-      },
+  },
+  {
+    path: "/about-us",
+    element: <AboutUs />
+  },
+  {
+    path: "/contact-us",
+    element: <ContactUs />
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router = { router}/>
+    <RouterProvider router={router} />
 
   </React.StrictMode>,
 )
